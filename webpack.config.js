349446383deb67605dcf002efe83e55b77a8a0ec
Kristie Desiree <kly5@mail.sfsu.edu>
2019-06-12 22:38:20 -0700

    Specify output file path + name including hash

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
+const path = require('path');
+
 module.exports = {
   mode: 'development',
-  entry: './src/index.js'
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].bundle.js'
+  }
 }
