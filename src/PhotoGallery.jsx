86ce1d4e6230202fe8016fa21bbd805e09baa893
Kristie Desiree <kly5@mail.sfsu.edu>
2019-06-16 14:27:07 -0700

    Create a photogallery "container" component

---
+++ src/PhotoGallery.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+let PhotoGallery = (props) => {
+  let { images, selectedImage } = props;
+
+  return (
+    <div>
+      This is my gallery "container" component
+    </div>
+  )
+  
+}
+
+export default PhotoGallery;
