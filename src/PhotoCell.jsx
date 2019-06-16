c3cf30c8df2922c8692fa79f21145ed553225cfb
Kristie Desiree <kly5@mail.sfsu.edu>
2019-06-16 14:37:05 -0700

    Create PhotoCell presentational component

---
+++ src/PhotoCell.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+let PhotoCell = (props) => {
+  let { selectedImage } = props;
+
+  return (
+    <div>
+     This is a photo cell component
+    </div>
+  )
+  
+}
+
+export default PhotoCell;
