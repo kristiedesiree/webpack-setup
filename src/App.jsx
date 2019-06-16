526d6bf317174df409661cc51ac860d23a04582a
Kristie Desiree <kly5@mail.sfsu.edu>
2019-06-16 14:16:07 -0700

    Create componentDidMount method and set the state to be the sample images array and a selected image

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import sampleImages from './sampleImageGenerator.js'
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -16,6 +16,14 @@ class App extends React.Component {
       Gallery component will be rendered within the app component
     </div>
   );
+  }
+
+  componentDidMount() {
+    this.setState({images: sampleImages, selectedImage: sampleImages[0]}, () => {
+      console.log(this.state.images)
+      console.log(this.state.selectedImage)
+    }
+    );
   }
 }
 
