cedf4051808e21cb8812d20a989845fd15e03283
Kristie Desiree <kly5@mail.sfsu.edu>
2019-06-16 14:59:36 -0700

    Invoke first render after componentDidMount

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,23 @@ class App extends React.Component {
     }
   }
 
-  render() {
-  return (
-    <div>
-      Rendered the App component
-      Gallery component will be rendered within the app component
-      <PhotoGallery images={ this.state.images } selectedImage={ this.state.selectedImage } />
-    </div>
-  );
-  }
+
 
   componentDidMount() {
     this.setState({images: sampleImages, selectedImage: sampleImages[0]}, () => {
       console.log(this.state.images)
       console.log(this.state.selectedImage)
     }
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        Rendered the App component
+        {/* Gallery component will be rendered within the app component */}
+        <PhotoGallery images={ this.state.images } selectedImage={ this.state.selectedImage } />
+      </div>
     );
   }
 }
