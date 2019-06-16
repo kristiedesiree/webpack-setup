5425974f0a6bc06a96813ccf0858484e8a48e46b
Kristie Desiree <kly5@mail.sfsu.edu>
2019-06-16 13:40:19 -0700

    Create shell of upper level App component

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     images: []
+  //   }
+  // }
+
+  render() {
+  return (
+    <div>
+      Rendered the App component
+      Gallery component will be rendered within the app component
+    </div>
+  );
+  }
+}
+
+export default App;
