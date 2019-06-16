98750eacfc419c0f69292f35457b08fe31f3ecf8
Kristie Desiree <kly5@mail.sfsu.edu>
2019-06-16 14:37:32 -0700

    Map over images array to create PhotoCell for each image

--- src/PhotoGallery.jsx
+++ src/PhotoGallery.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PhotoCell from './PhotoCell.jsx';
 
 let PhotoGallery = (props) => {
   let { images, selectedImage } = props;
@@ -6,6 +7,10 @@ let PhotoGallery = (props) => {
   return (
     <div>
       This is my gallery "container" component
+      <PhotoCell image={selectedImage} />
+      {images.map((image, index) => {
+        <PhotoCell key={index} image={image} />
+      })}
     </div>
   )
   
