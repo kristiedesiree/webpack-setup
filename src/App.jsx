11d98e367ba4cf5caf097d83dde9e8d5d1a79d67
Kristie Desiree <kly5@mail.sfsu.edu>
2019-06-16 13:49:22 -0700

    Add constructor to App class

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     images: []
-  //   }
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [],
+      selectedImage: {}
+    }
+  }
 
   render() {
   return (
