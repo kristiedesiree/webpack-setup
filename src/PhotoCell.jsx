e2f38824ee83f1cf16726e69a7fd82073df7e719
Kristie Desiree <kly5@mail.sfsu.edu>
2019-06-16 19:42:53 -0700

    Conditionally render only the cell that matches the selectedImageID

--- src/PhotoCell.jsx
+++ src/PhotoCell.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
 let PhotoCell = (props) => {
-  let { image } = props;
-
-  return (
+  let { id, image, selectedImage, selectedImageId } = props;
+  console.log('selecId', selectedImageId)
+  if (id === selectedImageId) {
+    return (
     <div>
-     This is a photo cell component
      <p> {image.caption} </p>
      <img src={image.url}/>
     </div>
-  )
+  )  
+  } else {
+    return ''
+  }
   
 }
 
