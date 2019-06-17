85c926c286dad0b2020660274082d85d45352f02
Kristie Desiree <kly5@mail.sfsu.edu>
2019-06-16 19:44:00 -0700

    Pass state down to PhotoGallery component as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import sampleImages from './sampleImageGenerator.js';
 import PhotoGallery from './PhotoGallery.jsx';
-import PhotoCell from './PhotoCell.jsx';
-
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       images: [],
-      selectedImage: {}
+      selectedImage: {},
+      selectedImageId: null,
     }
   }
 
-
-
   componentDidMount() {
-    this.setState({images: sampleImages, selectedImage: sampleImages[0]}, () => {
+    this.setState({images: sampleImages, selectedImage: sampleImages[0], selectedImageId: 0}, () => {
       console.log(this.state.images)
       console.log(this.state.selectedImage)
     }
@@ -27,11 +24,13 @@ class App extends React.Component {
     return (
       <div>
         Rendered the App component
-        {/* Gallery component will be rendered within the app component */}
-        <PhotoGallery images={ this.state.images } selectedImage={ this.state.selectedImage } />
+        <PhotoGallery images={ this.state.images } selectedImage={ this.state.selectedImage } selectedImageId = {this.state.selectedImageId}/>
       </div>
     );
   }
+  
+
+  
 }
 
 export default App;
