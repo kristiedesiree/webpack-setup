cd14351345bf18e3f517fc5943da28e7ee616082
Kristie Desiree <kly5@mail.sfsu.edu>
2019-06-16 23:32:13 -0700

    Add components into flexbox

--- src/PhotoGallery.jsx
+++ src/PhotoGallery.jsx
@@ -1,29 +1,20 @@
 import React from 'react';
 import PhotoCell from './PhotoCell.jsx';
-// import { Gallery, GalleryImage } from "react-gesture-gallery";
-import './swipe.scss'
+import './style.scss'
+import Flexbox from 'flexbox-react';
 
 let PhotoGallery = (props) => {
   let { images, selectedImage, selectedImageId, clickHandler, getDragCoordinate} = props;
  
   return (
-    <div>
-       <button id= "left" className="navigate" onClick={() => {clickHandler('left')}}> LEFT </button>
+      <Flexbox className="gallery" flexDirection="row" justifyContent="center" alignItems="center">
+       <button id= "left" className="navigate" onClick={() => {clickHandler('left')}}> {'<'} </button>
         {images.map((image, index) => {
            return <PhotoCell id={ index } key={ index } image={ image } selectedImage={ selectedImage } selectedImageId={ selectedImageId } getDragCoordinate={getDragCoordinate} />
         })}  
-       <button id="right" className="navigate" onClick={() => {clickHandler('right')}}> RIGHT </button>
-      Gallery here
-<div>
-
-</div>
-
-</div>
-
-    
+       <button id="right" className="navigate" onClick={() => {clickHandler('right')}}> {'>'} </button>
+    </Flexbox>
   )
-
-
 }
 
 export default PhotoGallery;
