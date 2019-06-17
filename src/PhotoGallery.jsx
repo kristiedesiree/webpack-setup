a5847dee4fbfff59887919a202e4753a3df8cd67
Kristie Desiree <kly5@mail.sfsu.edu>
2019-06-16 22:03:39 -0700

    Pass handlers to photocell component

--- src/PhotoGallery.jsx
+++ src/PhotoGallery.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import PhotoCell from './PhotoCell.jsx';
-
+// import { Gallery, GalleryImage } from "react-gesture-gallery";
+import './swipe.scss'
 
 let PhotoGallery = (props) => {
-  let { images, selectedImage, selectedImageId, clickHandler } = props;
-  console.log(clickHandler)
+  let { images, selectedImage, selectedImageId, clickHandler, getDragStart, getDragEnd } = props;
+ 
   return (
     <div>
-      <button id= "left" className="navigate" onClick={() => {clickHandler('left')}}> LEFT </button>
-       {images.map((image, index) => {
-          return <PhotoCell id={ index } key={ index } image={ image } selectedImage={ selectedImage } selectedImageId={ selectedImageId }/>
-       })}  
-      <button id="right" className="navigate" onClick={() => {clickHandler('right')}}> RIGHT </button>
+       <button id= "left" className="navigate" onClick={() => {clickHandler('left')}}> LEFT </button>
+        {images.map((image, index) => {
+           return <PhotoCell id={ index } key={ index } image={ image } selectedImage={ selectedImage } selectedImageId={ selectedImageId } getDragStart={getDragStart} getDragEnd={getDragEnd}/>
+        })}  
+       <button id="right" className="navigate" onClick={() => {clickHandler('right')}}> RIGHT </button>
       Gallery here
-     
-    </div>
+<div>
+
+</div>
+
+</div>
+
+    
   )
-  
+
+
 }
 
 export default PhotoGallery;
