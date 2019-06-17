e566ce7be4867b0b328030f2d8c5699dac1e1fb6
Kristie Desiree <kly5@mail.sfsu.edu>
2019-06-16 19:57:50 -0700

    Apply clickhandler to onClick of buttons in PhotoGallery

--- src/PhotoGallery.jsx
+++ src/PhotoGallery.jsx
@@ -3,14 +3,15 @@ import PhotoCell from './PhotoCell.jsx';
 
 
 let PhotoGallery = (props) => {
-  let { images, selectedImage, selectedImageId, drag } = props;
-
+  let { images, selectedImage, selectedImageId, clickHandler } = props;
+  console.log(clickHandler)
   return (
     <div>
+      <button id= "left" className="navigate" onClick={() => {clickHandler('left')}}> LEFT </button>
        {images.map((image, index) => {
           return <PhotoCell id={ index } key={ index } image={ image } selectedImage={ selectedImage } selectedImageId={ selectedImageId }/>
        })}  
-  
+      <button id="right" className="navigate" onClick={() => {clickHandler('right')}}> RIGHT </button>
       Gallery here
      
     </div>
